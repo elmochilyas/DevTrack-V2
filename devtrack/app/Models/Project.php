8e270d1d9ec878d95eeaaa62e621b0fd69861cf4
najwa13 <najwafathi05@hotmail.com>
2026-05-05 12:59:37 +0100

    feat: customize User model and implement project/task model

--- devtrack/app/Models/Project.php
+++ devtrack/app/Models/Project.php
@@ -3,8 +3,41 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    //
+    use SoftDeletes;
+
+     protected $fillable = [
+        'title',
+        'description',
+        'deadline',
+        'created_by',
+    ];
+
+    
+     protected $casts = [
+        'deadline' => 'datetime',
+    ];
+
+    //Get the user who created this project
+     public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    //Get all team members of this project
+    public function members()
+    {
+        return $this->belongsToMany(User::class, 'project_user')
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+
+    //Get all tasks in this project
+     public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- devtrack/app/Models/User.php
+++ devtrack/app/Models/User.php
@@ -10,23 +10,42 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+     protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    //Get all projects where user is a member or lead
+    public function projects()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->belongsToMany(Project::class, 'project_user')
+            ->withPivot('role')
+            ->withTimestamps();
+    }
+
+    //Get all tasks assigned to this user
+    public function tasksAssigned()
+    {
+        return $this->hasMany(Task::class, 'assigned_to');
+    }
+    
+    //Get all projects created by this user (as lead)
+    public function createdProjects()
+    {
+        return $this->hasMany(Project::class, 'created_by');
     }
 }
