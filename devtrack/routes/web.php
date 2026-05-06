6088f6c26f56a2145ae92101d8d431ba7cfcb485
najwa13 <najwafathi05@hotmail.com>
2026-05-06 14:56:40 +0100

    [ROUTING] Configure nested resource routes for projects and tasks

--- devtrack/routes/web.php
+++ devtrack/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('projects', ProjectController::class);
+    Route::resource('projects.tasks', TaskController::class);
+    
+    // Custom route for status-only updates
+    Route::put('projects/{project}/tasks/{task}/status', [TaskController::class, 'updateStatus'])
+        ->name('tasks.updateStatus');
+
 });
 
 require __DIR__.'/auth.php';
